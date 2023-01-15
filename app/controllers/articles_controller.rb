556dbe668f42ffb108bf46a4ea17bfcda0ccfdf6
Glen Pike <github@glenpike.co.uk>
2023-01-15 23:33:08 +0000

    Sorting: Allow sorting of articles
    - Simple array sorting in controller with spec's
    - Style tweaks - colour styling buttons and spacing, etc.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,17 @@ class ArticlesController < ApplicationController
   private
     def articles
       @articles ||= Article.all_with_likes
+
+      case params[:sort]
+      when 'title'
+        @articles.sort { |a, b| a.title <=> b.title }
+      when 'closest'
+        @articles.sort { |a, b| a.location.distance <=> b.location.distance }
+      when 'popular'
+        @articles.sort { |a, b| a.reactions.views <=> b.reactions.views }
+      else
+        @articles
+      end
     end
 
     helper_method :articles
